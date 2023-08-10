56e6774aa5af82de56206e865aaf3c2d4b833d1d
H1MAA <111005028+H1MAA@users.noreply.github.com>
2023-08-10 22:13:36 +0300

    Project Skeleton

---
+++ Dorosak_dev/Core/ExcelHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML;
+
+namespace Dorosak_dev.Core
+{
+    internal class ExcelHandler
+    {
+        public ExcelHandler()
+        {
+        }
+
+
+    }
+}
